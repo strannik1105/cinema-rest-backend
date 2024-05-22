dd8001236765297032c9dad7b8113b9d78e38207
strannik1105 <danil.stolpovskikh@gmail.com>
2024-05-23 00:08:59 +0700

    feat(app): add CORSMiddleware

--- src/main.py
+++ src/main.py
@@ -1,10 +1,20 @@
 import uvicorn
 import settings
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from router.api import api_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(api_router)
 
 if __name__ == "__main__":
