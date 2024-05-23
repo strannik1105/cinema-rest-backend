518dc32f4330947781f5de057d93561e0b1ddef9
strannik1105 <danil.stolpovskikh@gmail.com>
2024-05-23 23:57:08 +0700

    fix(food-and-movies): fix missing columns

---
+++ src/migrations/versions/720f698b3fe8_add_price_and_genre.py
@@ -0,0 +1,25 @@
+"""Add price and genre
+
+Revision ID: 720f698b3fe8
+Revises: e523cddb5763
+Create Date: 2024-05-23 23:52:03.246512
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = '720f698b3fe8'
+down_revision = 'e523cddb5763'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.add_column("food", sa.Column("price", sa.Float, nullable=False), schema="food")
+    op.add_column("movie", sa.Column("genre", sa.String), schema="movies")
+
+
+def downgrade() -> None:
+    op.drop_column("movie", "genre", schema="genre")
+    op.drop_column("food", "price", schema="food")
