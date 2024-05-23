168cbf12e670d98f5fc4e366bff70b56f0305562
strannik1105 <danil.stolpovskikh@gmail.com>
2024-05-23 23:18:17 +0700

    feat(deploy): add .env.deploy

--- src/settings.py
+++ src/settings.py
@@ -19,23 +19,26 @@ class AuthJwt(BaseModel):
     access_token_expire_minutes: int = 15
 
 
-def get_local_host():
-    match platform:
-        case "linux" | "darwin":
-            return "0.0.0.0"
-        case "win32":
-            return "localhost"
-        case _:
-            raise OSError("Unsupported platform")
+def get_local_host(addr="0.0.0.0"):
+    if addr in ("0.0.0.0", "localhost", "127.0.0.1"):
+        match platform:
+            case "linux" | "darwin":
+                return "0.0.0.0"
+            case "win32":
+                return "localhost"
+            case _:
+                raise OSError("Unsupported platform")
+    else:
+        return addr
 
 
-HOST = get_local_host()
+HOST = get_local_host(os.getenv("HOST", "0.0.0.0"))
 PORT: int = int(os.getenv("PORT", 8000))
 
 POSTGRES_DB: str = os.getenv("POSTGRES_DB", "events")
 POSTGRES_USER: str = os.getenv("POSTGRES_USER", "user")
 POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD", "password")
-POSTGRES_HOST: str = os.getenv("POSTGRES_HOST", get_local_host())
+POSTGRES_HOST: str = get_local_host(os.getenv("POSTGRES_HOST", "0.0.0.0"))
 POSTGRES_PORT: int = int(os.getenv("POSTGRES_PORT", "5432"))
 FILE_PATH: str = os.getenv("FILE_PATH", "../static")
 
